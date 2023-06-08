a3f6bd9cc2d4faf1df50a907c77d1b618034fca3
anthony <aalbertini95@gmail.com>
2023-06-08 09:03:43 +0200

    adds some checks

--- src/components/CalendarEvents/index.tsx
+++ src/components/CalendarEvents/index.tsx
@@ -9,6 +9,41 @@ import { hasEventCollision } from "@/utils/events";
 export default function CalendarEvents() {
   const [events, setEvents] = useState<CalendarEvent[]>(CalendarEventsData);
 
+  function layOutDay(events: CalendarEvent[]) {
+    // we check if events is an array
+    if (!Array.isArray(events)) {
+      throw new Error("events must be an array");
+    }
+
+    // we check if it has a start and end property, and if both are numbers
+    events.forEach((event) => {
+      if (typeof event.start !== "number" || typeof event.end !== "number") {
+        throw new Error(
+          "Each event must be an object with 'start' and 'end' properties of type number"
+        );
+      }
+
+      if (
+        event.start < 0 ||
+        event.start >= 720 ||
+        event.end < 0 ||
+        event.end > 720
+      ) {
+        throw new Error(
+          "'start' and 'end' properties must be non-negative and within the range 0-720"
+        );
+      }
+
+      if (event.end <= event.start) {
+        throw new Error("'end' property must be greater than 'start' property");
+      }
+    });
+
+    // NOTE: Some validations (above) might be overkill in a TypeScript project but `layoutDay` is globally accessible.
+
+    setEvents(sortCalendarEvents(events));
+  }
+
   // Allow to put layOutDay in the global namespace.
   // To change the state of the events, you can call the layOutDay function like that in the browser's console:
   // - layOutDay.call(null, []);
@@ -19,9 +54,7 @@ export default function CalendarEvents() {
   // - layOutDay.apply(null, [[{ start: 30, end: 150 }, { start: 540, end: 600 }]]);
   // ...
   useEffect(() => {
-    globalThis.layOutDay = function (events: CalendarEvent[]) {
-      setEvents(sortCalendarEvents(events));
-    };
+    globalThis.layOutDay = layOutDay;
   }, []);
 
   function renderEvents() {
